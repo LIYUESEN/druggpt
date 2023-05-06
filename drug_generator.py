add85bb2d283a89515a4f446abc96c4f29ba4e71
Wang Xiangyu <132470925+xjtu-wangxiangyu@users.noreply.github.com>
2023-05-06 23:57:00 +0800

    Some code optimizations

--- drug_generator.py
+++ drug_generator.py
@@ -6,50 +6,95 @@ Created on Mon May  1 19:41:07 2023
 """
 
 import os
+import sys
 import subprocess
+import hashlib
 import warnings
+import csv
 from tqdm import tqdm
 import argparse
 import torch
 from transformers import AutoTokenizer, GPT2LMHeadModel
 
-warnings.filterwarnings('ignore')
-#Sometimes, using Hugging Face may require a proxy.
-#os.environ["http_proxy"] = "http://your.proxy.server:port"
-#os.environ["https_proxy"] = "http://your.proxy.server:port"
 
+class LigandPostprocessor:
+    def __init__(self, path):
+        self.hash_ligand_mapping = {}
+        self.output_path = path  # Output directory for SDF files
+        self.load_mapping()
 
-# Set up command line argument parsing
-parser = argparse.ArgumentParser()
-parser.add_argument('-p', type=str, default=None, help='Input the protein amino acid sequence. Default value is None. Only one of -p and -f should be specified.')
-parser.add_argument('-f', type=str, default=None, help='Input the FASTA file. Default value is None. Only one of -p and -f should be specified.')
-parser.add_argument('-l', type=str, default='', help='Input the ligand prompt. Default value is an empty string.')
-parser.add_argument('-n', type=int, default=100, help='Number of output molecules to generate. Default value is 100.')
-parser.add_argument('-d', type=str, default='cuda', help="Hardware device to use. Default value is 'cuda'.")
-parser.add_argument('-o', type=str, default='./ligand_output/', help="Output directory for generated molecules. Default value is './ligand_output/'.")
+    def load_mapping(self):
+        mapping_file = os.path.join(output_path, 'hash_ligand_mapping.csv')
+        if os.path.exists(mapping_file):
+            print("Found existed mapping file, now reading ...")
+            with open(mapping_file, 'r') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    self.hash_ligand_mapping[row[0]] = row[1]
 
-args = parser.parse_args()
+    # Define a function to save the hash-ligand mapping to a file
+    def save_mapping(self):
+        mapping_file = os.path.join(output_path, 'hash_ligand_mapping.csv')
+        with open(mapping_file, 'w', newline='') as f:
+            writer = csv.writer(f)
+            for ligand_hash, ligand in self.hash_ligand_mapping.items():
+                writer.writerow([ligand_hash, ligand])
 
-protein_seq = args.p
-fasta_file = args.f
-ligand_prompt = args.l
-num_generated = args.n
-device = args.d
-output_path = args.o
+    # Define a function to filter out empty SDF files
+    def filter_sdf(self, hash_ligand_mapping_per_batch):
+        print("Filtering sdf ...")
+        ligand_hash_list = list(hash_ligand_mapping_per_batch.keys())
+        for ligand_hash in tqdm(ligand_hash_list):
+            filepath = os.path.join(self.output_path, ligand_hash + '.sdf')
+            with open(filepath, 'r') as f:
+                text = f.read()
+            if len(text) < 2:
+                os.remove(filepath)
+                hash_ligand_mapping_per_batch.pop(ligand_hash)
+        return hash_ligand_mapping_per_batch
 
+    # Define a function to generate SDF files from a list of ligand SMILES using OpenBabel
+    def to_sdf(self, ligand_list_per_batch):
+        print("Converting to sdf ...")
+        hash_ligand_mapping_per_batch = {}
+        for ligand in tqdm(ligand_list_per_batch):
+            ligand_hash = hashlib.sha1(ligand.encode()).hexdigest()
+            if ligand_hash not in self.hash_ligand_mapping.keys():
+                filepath = self.output_path + ligand_hash + '.sdf'
+                cmd = "obabel -:" + ligand + " -osdf -O " + filepath + " --gen3d --forcefield mmff94"
+                try:
+                    subprocess.check_output(cmd, timeout=10, stderr=subprocess.DEVNULL)
+                except subprocess.TimeoutExpired:
+                    pass
+                finally:
+                    if os.path.exists(filepath):
+                        hash_ligand_mapping_per_batch[
+                            ligand_hash] = ligand  # Add the hash-ligand mapping to the dictionary
+        self.hash_ligand_mapping.update(self.filter_sdf(hash_ligand_mapping_per_batch))
 
-def ifno_mkdirs(dirname):
-    if not os.path.exists(dirname):
-        os.makedirs(dirname) 
+def about():
+    print("""
+  _____                    _____ _____ _______ 
+ |  __ \                  / ____|  __ \__   __|
+ | |  | |_ __ _   _  __ _| |  __| |__) | | |   
+ | |  | | '__| | | |/ _` | | |_ |  ___/  | |   
+ | |__| | |  | |_| | (_| | |__| | |      | |   
+ |_____/|_|   \__,_|\__, |\_____|_|      |_|   
+                     __/ |                     
+                    |___/                      
+ A generative drug design model based on GPT2
+    """)
 
-ifno_mkdirs(output_path)
+def ifno_mkdirs(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 # Function to read in FASTA file
 def read_fasta_file(file_path):
-    with open(file_path, 'r') as fasta_file:
+    with open(file_path, 'r') as f:
         sequence = []
-        
-        for line in fasta_file:
+
+        for line in f:
             line = line.strip()
             if not line.startswith('>'):
                 sequence.append(line)
@@ -58,83 +103,89 @@ def read_fasta_file(file_path):
 
     return protein_sequence
 
-# Check if the input is either a protein amino acid sequence or a FASTA file, but not both
-if (protein_seq is not None) != (fasta_file is not None):
-    if fasta_file is not None:
+if __name__ == "__main__":
+    about()
+    warnings.filterwarnings('ignore')
+    #Sometimes, using Hugging Face may require a proxy.
+    #os.environ["http_proxy"] = "http://your.proxy.server:port"
+    #os.environ["https_proxy"] = "http://your.proxy.server:port"
+
+    # Set up command line argument parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', type=str, default=None, help='Input a protein amino acid sequence. Default value is None. Only one of -p and -f should be specified.')
+    parser.add_argument('-f', type=str, default=None, help='Input a FASTA file. Default value is None. Only one of -p and -f should be specified.')
+    parser.add_argument('-l', type=str, default='', help='Input a ligand prompt. Default value is an empty string.')
+    parser.add_argument('-n', type=int, default=100, help='At least how many molecules will be generated. Default value is 100.')
+    parser.add_argument('-d', type=str, default='cuda', help="Hardware device to use. Default value is 'cuda'.")
+    parser.add_argument('-o', type=str, default='./ligand_output/', help="Output directory for generated molecules. Default value is './ligand_output/'.")
+    parser.add_argument('-b', type=int, default=64, help="How many molecules will be generated per batch. Try to reduce this value if you have low RAM. Default value is 64.")
+
+    args = parser.parse_args()
+    protein_seq = args.p
+    fasta_file = args.f
+    ligand_prompt = args.l
+    num_generated = args.n
+    device = args.d
+    output_path = args.o
+    batch_generated_size = args.b
+
+    # Check if the input is either a protein amino acid sequence or a FASTA file, but not both
+    if (not protein_seq) and (not fasta_file):
+        print("Error: Input is empty.")
+        sys.exit(1)
+    if protein_seq and fasta_file:
+        print("Error: The input should be either a protein amino acid sequence or a FASTA file, but not both.")
+        sys.exit(1)
+    if fasta_file:
         protein_seq = read_fasta_file(fasta_file)
-    else:
-        protein_seq = protein_seq
-else:
-    print("The input should be either a protein amino acid sequence or a FASTA file, but not both.")
 
-# Load the tokenizer and the model
-tokenizer = AutoTokenizer.from_pretrained('liyuesen/druggpt')
-model = GPT2LMHeadModel.from_pretrained("liyuesen/druggpt")
+    # Load the tokenizer and the model
+    tokenizer = AutoTokenizer.from_pretrained('liyuesen/druggpt')
+    model = GPT2LMHeadModel.from_pretrained("liyuesen/druggpt")
 
-# Generate a prompt for the model
-p_prompt = "<|startoftext|><P>" + protein_seq + "<L>"
-l_prompt = "" + ligand_prompt 
-prompt = p_prompt + l_prompt
-print(prompt)
+    # Generate a prompt for the model
+    p_prompt = "<|startoftext|><P>" + protein_seq + "<L>"
+    l_prompt = "" + ligand_prompt
+    prompt = p_prompt + l_prompt
+    print(prompt)
 
-# Move the model to the specified device
-model.eval()
-device = torch.device(device)
-model.to(device)
+    # Move the model to the specified device
+    model.eval()
+    device = torch.device(device)
+    model.to(device)
 
+    # Create a LigandPostprocessor object
+    ligand_post_processor = LigandPostprocessor(output_path)
 
+    # Generate molecules
+    generated = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0)
+    generated = generated.to(device)
 
-#Define post-processing function
-#Define function to generate SDF files from a list of ligand SMILES using OpenBabel
-def get_sdf(ligand_list,output_path):
-    for ligand in tqdm(ligand_list):
-        filename = output_path + 'ligand_' + ligand +'.sdf'
-        cmd = "obabel -:" + ligand + " -osdf -O " + filename + " --gen3d --forcefield mmff94"# --conformer --nconf 1 --score rmsd
-        #subprocess.check_call(cmd, shell=True)
-        try:
-            output = subprocess.check_output(cmd, timeout=10)
-        except subprocess.TimeoutExpired:
-            pass
-#Define function to filter out empty SDF files
-def filter_sdf(output_path):
-    filelist = os.listdir(output_path)
-    for filename in filelist:
-        filepath = os.path.join(output_path,filename)
-        with open(filepath,'r') as f:
-            text = f.read()
-        if len(text)<2:
-            os.remove(filepath)
+    batch_number = 0
+    ifno_mkdirs(output_path)
 
+    while len(os.listdir(output_path))<num_generated:
+        generate_ligand_list = []
+        batch_number += 1
+        print(f"Batch {batch_number}")
+        print("Generating ligand SMILES ...")
+        sample_outputs = model.generate(
+                                        generated,
+                                        #bos_token_id=random.randint(1,30000),
+                                        do_sample=True,
+                                        top_k=5,
+                                        max_length = 1024,
+                                        top_p=0.6,
+                                        num_return_sequences=batch_generated_size
+                                        )
 
-
-
-# Generate molecules
-generated = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0)
-generated = generated.to(device)
-
-
-for i in range(100):
-    ligand_list = []
-    sample_outputs = model.generate(
-                                    generated, 
-                                    #bos_token_id=random.randint(1,30000),
-                                    do_sample=True,   
-                                    top_k=5, 
-                                    max_length = 1024,
-                                    top_p=0.6, 
-                                    num_return_sequences=64
-                                    )
-
-    for sample_output in sample_outputs:
-        ligand_list.append(tokenizer.decode(sample_output, skip_special_tokens=True).split('<L>')[1])
-    torch.cuda.empty_cache()
-      
-    get_sdf(ligand_list,output_path)
-    filter_sdf(output_path)
-
-    if len(os.listdir(output_path))>num_generated:
-        break
-    else:pass
+        for sample_output in sample_outputs:
+            generate_ligand_list.append(tokenizer.decode(sample_output, skip_special_tokens=True).split('<L>')[1])
+        torch.cuda.empty_cache()
+        ligand_post_processor.to_sdf(generate_ligand_list)
+    print("Saving mapping file ...")
+    ligand_post_processor.save_mapping()
+    print(f"{len(ligand_post_processor.hash_ligand_mapping)} molecules successfully generated!")
 
 
 
